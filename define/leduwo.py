8006686306fbde4b0549892857a29de492bcb758
Hyello Songer <hyello@163.com>
2016-07-28 23:19:31 +0800

    对新的snwx协议和leduwo协议的阶段性更改

--- define/leduwo.py
+++ define/leduwo.py
@@ -1,7 +1,24 @@
 # -*- coding: utf-8 -*-
+#leduwo网站使用gbk编码
+ENCODING = 'gbk'
 
-def getcontext():
-	pass
+import kit
+from urllib.request import urlopen
+
+def getcontext(url):
+	'''获取书籍目录，参数为目录页面，返回书籍名与一个由二项元组组成的列表，元组第一项为链接，第二项为章节标题'''
+	kit.logger(0, '打开小说目录页(%s)' % url)
+	with urlopen(url) as file_obj:
+		list_raw = file_obj.read()
+		list_str = list_raw.decode(ENCODING, 'replace')
+	#kit.pickout(list_str, '<div id=\"box\"><h1>', '</h1></div>') 书名
+	#kit.pickout(list_str, '<a href=\"http://www.leduwo.com/modules/article/search.php?searchtype=author&searchkey=', '\" target=\"_blank\">')  作者
+	step1 = kit.pickout(list_str, '<ul>', '</ul>')
 
 def getchapter():
+	'''获取章节内容，参数为该章节页面，返回章节内容（html）'''
+	pass
+
+def search(keyword):
+	'''搜索书籍，参数为关键字，返回一个由二项元组组成的列表，元组第一项为链接，第二项为书籍名称'''
 	pass
--- define/snwx.py
+++ define/snwx.py
@@ -1,9 +1,12 @@
 # -*- coding: utf-8 -*-
+#snwx使用gbk编码
+ENCODING = 'gbk'
+
 import kit
 from urllib.request import urlopen
 
 def getcontext(url):
-	'''获取书籍目录，参数为目录页面，返回书籍名与一个由二项元组组成的列表，元组第一项为链接，第二项为章节标题'''
+	'''获取书籍目录，参数为目录页面，返回书籍名，作者，书籍id与一个由二项元组组成的列表，元组第一项为链接，第二项为章节标题'''
 	kit.logger(0, '打开小说目录页(%s)' % url)
 	with urlopen(url) as file_obj:
 		list_raw = file_obj.read()
@@ -18,6 +21,14 @@ def getcontext(url):
 			kit.logger(1, '割目录结构出错(%s)' % i)
 		#print(step2[len(step2) - 1][0], step2[len(step2) - 1][1])
 	kit.logger(0, '整理完毕')
-	return kit.pickout(list_str, '<DIV id="BookMl">', '<BR>'), step2
+	return kit.pickout(list_str, 'var booktitle = \"', '\";'), \
+	kit.pickout(list_str, 'var bookid = \"', '\";'), \
+	kit.pickout(list_str, '<i>作者：', '</i>'), step2
+
 def getchapter(url):
+	'''获取章节内容，参数为该章节页面，返回章节内容（html）'''
 	return kit.pickout(urlopen(url).read().decode('gbk', 'replace'), '<div id=\"BookText\">', '</div>').replace('<br /><br />', '<br />') + '\n'
+
+def search(keyword):
+	'''搜索书籍，参数为关键字，返回一个由二项元组组成的列表，元组第一项为链接，第二项为书籍名称'''
+	pass
